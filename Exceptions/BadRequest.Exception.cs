b2d94bcfbe2aee9ed08277e80ff0b0cde29f8e13
Abhiram13 <abhiramnagadi@gmail.com>
2025-12-21 21:29:03 +0530

    updated exceptions

--- Exceptions/BadRequest.Exception.cs
+++ Exceptions/BadRequest.Exception.cs
@@ -2,5 +2,7 @@ namespace UrlShortner.Exceptions;
 
 public class BadRequestException : Exception
 {
+    public BadRequestException() : base () { }
     public BadRequestException(string message) : base (message) { }
+    public BadRequestException(string message, Exception innerException) : base(message, innerException) { }
 }
--- Exceptions/Unauthorised.Exception.cs
+++ Exceptions/Unauthorised.Exception.cs
@@ -2,5 +2,7 @@ namespace UrlShortner.Exceptions;
 
 public class UnauthorizedException : Exception
 {
+    public UnauthorizedException() : base () { }
     public UnauthorizedException(string message) : base (message) { }
+    public UnauthorizedException(string message, Exception innerException) : base (message, innerException) { }
 }
